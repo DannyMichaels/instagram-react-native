53e13f9f9eeddaeb8727ca526ffccd96a136095e
Daniel <itzdanielmichael@gmail.com>
2022-03-06 18:45:52 -0500

    persist in asyncstorage

--- store/useStore.js
+++ store/useStore.js
@@ -1,35 +1,48 @@
 import create from 'zustand';
-import { devtools } from 'zustand/middleware';
+import { devtools, persist } from 'zustand/middleware';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+const ASYNC_STORAGE_COMMENTS_KEY = ' ASYNC_STORAGE_COMMENTS_KEY';
 
 export const useStore = create(
-  devtools((set, get) => ({
-    allComments: {},
-    showCommentModal: false,
-    selectedItemId: null,
-
-    openCommentScreen: (id) => {
-      set({
-        showCommentModal: true,
-        selectedItemId: id,
-      });
-    },
-
-    closeCommentScreen: () => {
-      set({
+  devtools(
+    persist(
+      (set, get) => ({
+        allComments: {},
         showCommentModal: false,
         selectedItemId: null,
-      });
-    },
 
-    addComment: (newComment) => {
-      const currentPostComments = get().allComments[get().selectedItemId] || [];
-
-      set((state) => ({
-        allComments: {
-          ...state.allComments,
-          [state.selectedItemId]: [...currentPostComments, newComment] || [],
+        openCommentScreen: (id) => {
+          set({
+            showCommentModal: true,
+            selectedItemId: id,
+          });
         },
-      }));
-    },
-  }))
+
+        closeCommentScreen: () => {
+          set({
+            showCommentModal: false,
+            selectedItemId: null,
+          });
+        },
+
+        addComment: (newComment) => {
+          const currentPostComments =
+            get().allComments[get().selectedItemId] || [];
+
+          set((state) => ({
+            allComments: {
+              ...state.allComments,
+              [state.selectedItemId]:
+                [...currentPostComments, newComment] || [],
+            },
+          }));
+        },
+      }),
+      {
+        name: ASYNC_STORAGE_COMMENTS_KEY,
+        getStorage: () => AsyncStorage,
+      }
+    )
+  )
 );
